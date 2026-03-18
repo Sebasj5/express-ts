7bfd002446928dbd30632f2979ed5a396785deb8
Sebasj5 <satarij5@gmail.com>
2026-03-17 19:52:29 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import type { Request, Response } from 'express'
+import diaryRouter from './routes/diaries'
+const app = express()
+app.use(express.json())
+app.use('/api/diaries', diaryRouter)
+const PORT = 3000
+
+app.get('/ping', (_req: Request, res: Response) => {
+    console.log('someone pinged here')
+    res.send('pong')
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`)
+})
